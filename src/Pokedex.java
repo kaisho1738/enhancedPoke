e718fbb7d0a1a61d2f78088fb46a37e449101cb8
Pokedex <orichimaru619@gmail.com>
2025-06-23 22:24:13 +0800

    version 1

---
+++ src/Pokedex.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+public class Pokedex {
+    public static enum Type{
+        NORMAL, FIRE, WATER, ELECTRIC, GRASS, ICE, FIGHTING, POISON, GROUND, FLYING, PSYCHIC,
+        BUG, ROCK, GHOST, DRAGON, DARK, STEEL, FAIRY
+    }
+    public static enum MoveClass{ HM, TM }
+    public static Pokemon[] pokemon = new Pokemon[100];
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int option = -1;
+        printTitle();
+        menu();
+    }
+
+    public static void printTitle(){
+        System.out.println("\n" +
+                "██████╗  ██████╗ ██╗  ██╗███████╗██████╗ ███████╗██╗  ██╗    ███████╗██╗███╗   ███╗██╗   ██╗██╗      █████╗ ████████╗ ██████╗ ██████╗ \n" +
+                "██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝██╔══██╗██╔════╝╚██╗██╔╝    ██╔════╝██║████╗ ████║██║   ██║██║     ██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗\n" +
+                "██████╔╝██║   ██║█████╔╝ █████╗  ██║  ██║█████╗   ╚███╔╝     ███████╗██║██╔████╔██║██║   ██║██║     ███████║   ██║   ██║   ██║██████╔╝\n" +
+                "██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██║  ██║██╔══╝   ██╔██╗     ╚════██║██║██║╚██╔╝██║██║   ██║██║     ██╔══██║   ██║   ██║   ██║██╔══██╗\n" +
+                "██║     ╚██████╔╝██║  ██╗███████╗██████╔╝███████╗██╔╝ ██╗    ███████║██║██║ ╚═╝ ██║╚██████╔╝███████╗██║  ██║   ██║   ╚██████╔╝██║  ██║\n" +
+                "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝    ╚══════╝╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝\n" +
+                "                                                                                                                                      \n");
+        pause();
+        clearScreen();
+    }
+
+    public static void menu(){
+        int option = -1;
+        do{
+            System.out.println("Welcome to the Pokedex!");
+            System.out.println("0. Exit");
+            System.out.println("1.Pokedex Module");
+            System.out.println("2.Moves Module");
+            System.out.println("3.Items Module");
+            System.out.print("Which module would you like to access? ");
+            option = scanner.nextInt();
+            switch(option){
+                case 1:
+                    System.out.println("0. Exit");
+                    System.out.println("1.Add Pokemon");
+                    System.out.println("2.View all Pokemons");
+                    System.out.println("3.Search Pokemon");
+                    System.out.print("What do you want to do? ");
+                    option = scanner.nextInt();
+                    pokedexModule(option);
+                    break;
+            }
+        } while(option != 0);
+
+    }
+
+    public static void pokedexModule(int option){
+        switch(option){
+            case 1: //addpokemon
+            case 2: //viewpokemon
+            case 3: //searchpokemon
+        }
+    }
+
+
+    public static void addPokemon(){
+        System.out.println("Enter Pokedex Number: ");
+        int dexNum = scanner.nextInt();
+
+        System.out.println("Enter name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter");
+
+
+        System.out.println();
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[2J\033[H");
+        System.out.flush();
+    }
+
+    public static void pause() {
+        System.out.print("Press ENTER to continue . . .");
+        scanner.nextLine();
+    }
+}
---
+++ src/Pokemon.java
@@ -0,0 +1,121 @@
+public class Pokemon {
+
+    private int dexNum;
+    private String pokeName;
+    private Pokedex.Type type1;
+    private Pokedex.Type type2;
+    private int baseLevel;
+    private int evolvesFrom;
+    private int evolvesTo;
+    private int hp;
+    private int atk;
+    private int def;
+    private int spd;
+    private static int totalPokemons = 0;
+    private String cry;
+    private Move[] moves = new Move[10];
+    private int moveCount = 0;
+    //to add item object
+    //to add move object
+
+    //constructor - create pokemon with cry
+    public Pokemon(int dexNum, String pokeName, Pokedex.Type type1, Pokedex.Type type2, int baseLevel,
+                   int evolvesFrom, int evolvesTo, int hp, int atk, int def, int spd, String cry){
+        this.dexNum = dexNum;
+        this.pokeName = pokeName;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.baseLevel = baseLevel;
+        this.evolvesFrom = evolvesFrom;
+        this.evolvesTo = evolvesTo;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.spd = spd;
+        this.cry = cry;
+        totalPokemons++;
+    }
+
+    //pokemon w/o cry
+    public Pokemon(int dexNum, String pokeName, Pokedex.Type type1, Pokedex.Type type2, int baseLevel,
+                   int evolvesFrom, int evolvesTo, int hp, int atk, int def, int spd){
+        this.dexNum = dexNum;
+        this.pokeName = pokeName;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.baseLevel = baseLevel;
+        this.evolvesFrom = evolvesFrom;
+        this.evolvesTo = evolvesTo;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.spd = spd;
+        totalPokemons++;
+    }
+
+
+    //getters
+    public int getDexNum(){
+        return dexNum;
+    }
+    public String getPokeName(){
+        return pokeName;
+    }
+    public Pokedex.Type getType1(){
+        return type1;
+    }
+    public Pokedex.Type getType2(){
+        return type2;
+    }
+    public int getBaseLevel(){
+        return baseLevel;
+    }
+    public int getEvolvesFrom(){
+        return evolvesFrom;
+    }
+    public int getEvolvesTo(){
+        return evolvesTo;
+    }
+    public int getHp(){
+        return hp;
+    }
+    public int getAtk(){
+        return atk;
+    }
+    public int getDef(){
+        return def;
+    }
+    public int getSpd(){
+        return spd;
+    }
+    public String getCry(){return cry;}
+    public Move[] getMoves(){return moves;}
+
+    //setter to change cry
+    public void setCry(String cry){
+        this.cry = cry;
+    }
+
+    //methods; (1) view all pokemon to be implemented in main (2) search pokemon in main
+    public void addMove(Move move){
+        if(moveCount < moves.length){
+            for(int i = 0; i< moveCount;i++){
+                if(moves[i].getName().equalsIgnoreCase(move.getName())){
+                    System.out.println("Error: Move already exists!");
+                    return;
+                }
+            }
+            moves[moveCount++] = move;
+            System.out.println(move.getName() + " added successfully!");
+        } else{
+            System.out.println("Error: No more slots available!");
+        }
+    }
+
+    public void goCry(Pokemon pokemon){
+        System.out.println(pokemon.getCry());
+    }
+
+
+
+}
