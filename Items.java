3dab45be9afe320636d41000e4106cb6263111c9
thaeronconducto <151541085+thaeronconducto@users.noreply.github.com>
2025-06-27 10:08:23 +0800

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,70 @@
+/**
+ * This class represents an individual item. The attributes and constructors give a blueprint for the Item.
+ * The getters in this class allow other classes to access the assigned values to the attributes.
+ * There are currently no setters because there is not an option or a feature to change any of the Item's attributes
+ */
+
+public class Items {
+    private String name;
+    private String category;
+    private String description;
+    private String effect;
+    private int minBuyingPrice;
+    private int maxBuyingPrice;
+    private int sellingPrice;
+
+    /** constructor for move
+     * @param name              Name of the item
+     * @param category          Category of the item
+     * @param description       Description of the item
+     * @param effect            Effect/s of the item when held/used
+     * @param minBuyingPrice    Minimum buying price of the item
+     * @param maxBuyingPrice    Maximum buying price of the item
+     * @param sellingPrice      Selling price of the item
+     * */
+
+    public Items(String name, String category, String description, String effect,
+                 int minBuyingPrice, int maxBuyingPrice, int sellingPrice) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.effect = effect;
+        this.minBuyingPrice = minBuyingPrice;
+        this.maxBuyingPrice = maxBuyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    // Getters
+
+    public String getName() {
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getEffect() {
+        return effect;
+    }
+    public int getMinBuyingPrice() {
+        return minBuyingPrice;
+    }
+    public int getMaxBuyingPrice() {
+        return maxBuyingPrice;
+    }
+    public int getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public String getBuyPriceString() {
+        if (minBuyingPrice == 0 && maxBuyingPrice == 0) {
+            return "Not sold";
+        } else if (minBuyingPrice == maxBuyingPrice) {
+            return "₽" + minBuyingPrice;
+        } else {
+            return "₽" + minBuyingPrice + "–₽" + maxBuyingPrice;
+        }
+    }
+}
---
+++ ItemsDex.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * THINGS TO FIX:
+ * - Search method
+ * - initialized items
+ * - enum
+ * 
+ */
+
+/**
+ *
+ *
+ */
+
+public class ItemsDex {
+    public static Scanner scanner = new Scanner(System.in);
+
+    private List<Items> items = new ArrayList<>();
+
+    public enum Category {
+        VITAMIN,
+        LEVELING_ITEM,
+        FEATHER,
+        EVOLUTION_STONE
+
+    }
+    public void initializeItems() { // fix later
+        items.add(new Items("HP Up", "Vitamin", "A nutritious drink for Pokémon.", "+10 HP EVs", 10000, 10000, 5000));
+        items.add(new Items("Protein", "Vitamin", "A nutritious drink for Pokémon.", "+10 Attack EVs", 10000, 10000,5000));
+        items.add(new Items("Iron", "Vitamin", "A nutritious drink for Pokémon.", "+10 Defense EVs", 10000, 10000, 5000));
+        items.add(new Items("Carbos", "Vitamin", "A nutritious drink for Pokémon.", "+10 Speed EVs", 10000, 10000,5000));
+        items.add(new Items("Rare Candy", "Leveling Item", "A candy that is packed with energy.", "Increases level by 1", 0, 0,2400));
+        items.add(new Items("Health Feather", "Feather", "A feather that slightly increases HP.", "+1 HP EV", 300, 300,150));
+        items.add(new Items("Muscle Feather", "Feather", "A feather that slightly increases Attack.", "+1 Attack EV", 300, 300,150));
+        items.add(new Items("Resist Feather", "Feather", "A feather that slightly increases Defense.", "+1 Defense EV", 300, 300, 150));
+        items.add(new Items("Swift Feather", "Feather", "A feather that slightly increases Speed.", "+1 Speed EV", 300, 300, 150));
+        items.add(new Items("Zinc", "Vitamin", "A nutritious drink for Pokémon.", "+10 Special Defense EVs", 10000, 10000,5000));
+    }
+
+    public void searchItems() {
+        scanner.nextLine(); // consume leftover newline
+        System.out.print("Enter keyword to search: ");
+        String item = scanner.nextLine().toLowerCase();
+        for(Items toFind: items){
+            if(toFind.getName().equalsIgnoreCase(item)){
+                System.out.println("Move Found!");
+                viewItem(toFind);
+                return;
+            };
+        }
+        System.out.println("Move not Found!");
+        return;
+    }
+
+    public void viewItems(){
+        System.out.println("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+");
+        System.out.println("| Name             | Description                                                                           | Classification | Type One   | Type Two   |");
+        System.out.println("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+");
+        for(Items toPrint: items){
+            System.out.printf("| %-16s | %-85s | %-14s | %-10s | %-10s |\n",  toPrint.getName(), toPrint.getCategory(), toPrint.getDescription(),
+                    toPrint.getEffect(), toPrint.getBuyPriceString(), toPrint.getSellingPrice());
+            System.out.println("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+");
+        }
+    }
+
+    public void viewItem(Items items) {
+        System.out.println("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+");
+        System.out.println("| Name             | Description                                                                           | Classification | Type One   | Type Two   |");
+        System.out.println("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+");
+        System.out.printf("| %-16s | %-85s | %-14s | %-10s | %-10s |\n", items.getName(), items.getCategory(), items.getDescription(),
+                    items.getEffect(), items.getBuyPriceString(), items.getSellingPrice());
+        System.out.printf("+------------------+---------------------------------------------------------------------------------------+----------------+------------+------------+\n");
+    }
+
+
+/** represents all possible categories that an Item can be in */
+
+/** Prints a welcome statement for the move module.
+ *  CHANGE THIS TO ITEMS MODULE: di magawa-gawa ng ai ahhahaha
+ *  */
+public void printTitle() {
+    System.out.println("██╗████████╗███████╗███╗   ███╗███████╗    ███╗   ███╗ ██████╗ ██████╗ ██╗   ██╗██╗     ███████╗\n" +
+                       "██║╚══██╔══╝██╔════╝████╗ ████║██╔════╝    ████╗ ████║██╔═══██╗██╔══██╗██║   ██║██║     ██╔════╝\n" +
+                       "██║   ██║   █████╗  ██╔████╔██║███████╗    ██╔████╔██║██║   ██║██║  ██║██║   ██║██║     █████╗  \n" +
+                       "██║   ██║   ██╔══╝  ██║╚██╔╝██║╚════██║    ██║╚██╔╝██║██║   ██║██║  ██║██║   ██║██║     ██╔══╝  \n" +
+                       "██║   ██║   ███████╗██║ ╚═╝ ██║███████║    ██║ ╚═╝ ██║╚██████╔╝██████╔╝╚██████╔╝███████╗███████╗\n" +
+                       "╚═╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝╚══════╝\n" +
+                    "                                                                                                \n");
+    Pokedex.pause();
+}
+
+/** This method prints out the menu and is the main access/controller of the methods.
+ * This is the method that interacts with the user and calls out the appropriate methods accordingly.
+ * */
+public void menu() {
+    int option = -1;
+    do {
+        System.out.println("Welcome to the Items Module!");
+        System.out.println("0. Exit");
+        System.out.println("1. View Items");
+        System.out.println("2. Search Items");
+        System.out.print("What would you like to do?: ");
+        option = scanner.nextInt();
+
+        initializeItems();
+        switch (option) {
+            case 0:
+                System.out.println("");
+                System.out.println("Exiting...");
+                break;
+            case 1:
+                viewItems();
+                break;
+            case 2:
+                searchItems();
+                break;
+            default:
+                System.out.println("Error: Invalid Input");
+        }
+
+    } while (option != 0);
+
+}
+
+}
+
+
